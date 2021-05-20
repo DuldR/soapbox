7586cf86a451f14a89ce9953d7e85902b66b9f1c
Garrett <gnjulaka@gmail.com>
2021-05-20 17:58:23 -0500

    JS loading correctly to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create]
+    resources :stories, only: [:create, :index, :show]
+    resources :comments, only: [:create, :index, :show]
+    resource :session, only: [:create, :destroy]
+  end
+
 end
