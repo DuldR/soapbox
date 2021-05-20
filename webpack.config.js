122e4d69c867bf17d489562d87becbafa9eae849
Garrett <gnjulaka@gmail.com>
2021-05-20 17:52:25 -0500

    Bundle working

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/soapbox.jsx",
+  output: {
+    path: '/home/garrett/Documents/TestBed/soapbox/app/assets/javascripts',
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
