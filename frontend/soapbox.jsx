60f0bcd3a169424ec46c53c53a46096d32df640a
Garrett <gnjulaka@gmail.com>
2021-05-21 15:03:04 -0500

    Setup working so far

--- frontend/soapbox.jsx
+++ frontend/soapbox.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Root } from './components/root'
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
-    ReactDOM.render( <Root />, root)
+    const store = configureStore();
+    const root = document.getElementById("root")
+    
+    ReactDOM.render( <Root store={store}/>, root)
 })
---
+++ frontend/store/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import rootReducer from '../reducers/root_reducer'
+
+
+const configureStore = (preloadedState = {}) => {
+    return createStore(rootReducer, preloadedState, applyMiddleware(logger, thunk))
+}
+
+export default configureStore
