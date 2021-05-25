c902da10699122776cc3e48169f3c5b4446062bb
Garrett <gnjulaka@gmail.com>
2021-05-24 20:23:55 -0500

    Story index setup

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,10 @@ import GreetingContainer from './home/greeting_container'
 import SignupFormContainer from './forms/signup_form_container'
 import LoginFormContainer from './forms/login_form_container'
 
+import StoryIndexContainer from './stories/story_index_container'
+
+import { Route } from 'react-router-dom'
+
 const App = () => (
     <div>
         <section className="landing-info">
@@ -11,9 +15,13 @@ const App = () => (
         </section>
 
         <section>
-
             <AuthRoute path={'/signup'} exact={true} component={SignupFormContainer} />
             <AuthRoute path={'/login'} exact={true} component={LoginFormContainer} />
+        </section>
+
+        <section className={"main-index"}>
+            {/* MNeed to protect this route */}
+            <Route exact path={'/'} component={StoryIndexContainer} />
 
         </section>
 
---
+++ frontend/components/stories/story_index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class StoryIndex extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <h1>Testing</h1>
+        )
+    }
+}
+
+export default StoryIndex
