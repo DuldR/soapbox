159a4bd5fc2854340dbab35efafd9af984880f34
Garrett <gnjulaka@gmail.com>
2021-06-13 22:20:03 -0500

    Ok, feeling better now. Use the scope methods and the controller to specify which follows to pull.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,5 +1,12 @@
 class Follow < ApplicationRecord
-    belongs_to :followable, class_name: "User"
+    belongs_to :followable
     belongs_to :follower, class_name: "User"
-    belongs_to :user
+
+
+    # belongs_to :followable, polymorphic: true
+    # belongs_to :follower, polymorphic: true
+
+
+    # Then in the controller call the necessary method using switch case
+    scope :followable_user, ->(id) { where(followable_id: id, followable_type: "User") }
 end
