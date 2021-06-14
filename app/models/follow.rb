97702054455b76992957e2faadfb81c29fcae089
Garrett <gnjulaka@gmail.com>
2021-06-14 13:21:27 -0500

    Yata. Add a where clause in the story has many association.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,12 +1,15 @@
 class Follow < ApplicationRecord
-    belongs_to :followable
+    # belongs_to :followable
+    # belongs_to :follower, class_name: "User"
+
+
+    belongs_to :followable, polymorphic: true
     belongs_to :follower, class_name: "User"
-
-
-    # belongs_to :followable, polymorphic: true
-    # belongs_to :follower, polymorphic: true
 
 
     # Then in the controller call the necessary method using switch case
     scope :followable_user, ->(id) { where(followable_id: id, followable_type: "User") }
+    scope :followable_story, ->(id) { where(followable_id: id, followable_type: "Story") }
+
+    scope :follower_user, ->(id) { where(follower_id: id, followable_type: "User") }
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -12,7 +12,8 @@ class Story < ApplicationRecord
     # PRobably need some code here for ocmments
 
     has_many :comments
+    
 
-    has_many :story_follows, foreign_key: "story_id", dependent: :destroy
-    has_many :followed_users, through: :story_follows, source: :user
+    has_many :follows, -> { where(followable_type: "Story") }, foreign_key: "followable_id", dependent: :destroy
+    has_many :followed_users, through: :follows, source: :follower
 end
