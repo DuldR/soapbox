be722c193e68d81d66a0b7e73ebd4de68f2044ac
Garrett <gnjulaka@gmail.com>
2021-07-11 21:17:09 -0500

    Topics loaded out. Need to add UI load.

--- frontend/components/topics/topic_index.jsx
+++ frontend/components/topics/topic_index.jsx
@@ -7,11 +7,32 @@ class TopicIndex extends React.Component {
     constructor(props) {
         super(props)
 
+        this.listTopics = this.listTopics.bind(this)
+    }
+
+    componentDidMount() {
+        this.props.fetchTopics()
+    }
+
+    listTopics() {
+        if (Object.keys(this.props.topics).length === 0) {
+            return <h1>loading</h1>
+        } else {
+            const topicIndex = this.props.topics.map((topic, idx) => {
+                return <li key={"topic-" + idx}>{topic.topic_name}</li>
+            })
+
+            return topicIndex
+
+        }
     }
 
     render() {
+
         return ( 
-            <h1>Weiner</h1>
+            <ul>
+                {this.listTopics()}
+            </ul>
         )
     }
 }
--- frontend/components/topics/topic_index_container.jsx
+++ frontend/components/topics/topic_index_container.jsx
@@ -1,14 +1,18 @@
 import { connect } from 'react-redux'
 import TopicIndex from './topic_index'
 
+import { fetchTopics } from '../../actions/topic_actions'
+
+
 
 
 const mapStateToProps = (state) => ({
 
+    topics: state.entities.topics
 })
  
 const mapDispatchToProps = dispatch => ({
-    
+    fetchTopics: () => dispatch(fetchTopics())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(TopicIndex)
