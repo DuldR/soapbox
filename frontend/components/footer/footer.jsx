16ecc4e2e8daf5c90e33199141b20b3eb9c7ec80
Garrett <gnjulaka@gmail.com>
2021-07-18 20:41:53 -0500

    Footer setup. need to add real links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -2,12 +2,47 @@ import React from 'react'
 
 export const Footer = (props) => {
     return <ul className={"dashboard-footer"}>
-        <li>Weiner</li>
-        <li>Weiner</li>
+        <li>
+            <a href="#" >
+                Help
+            </a>
+        </li>
+        <li>
+            <a href="#" >
+                Status
+            </a>
+        </li>
+        <li>
+            <a href="#" >
+                Writers
+            </a>
+        </li>
+        <li>
+            <a href="#" >
+                Blog
+            </a>
+        </li>
+        <li>
+            <a href="#" >
+                Careers
+            </a>
+        </li>
+        <li>
+            <a href="#" >
+                Privacy
+            </a>
+        </li>
+        <li>
+            <a href="#" >
+                Terms
+            </a>
+        </li>
+        <li>
+            <a href="#" >
+                About
+            </a>
+        </li>
 
-        <li>Weiner</li>
-        <li>Weiner</li>
-        <li>Weiner</li>
 
     </ul>
 }
