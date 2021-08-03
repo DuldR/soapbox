fe28182eba61a25facbfe8ac24247e3150db3436
Garrett <gnjulaka@gmail.com>
2021-08-03 09:59:34 -0500

    Logo looking better

--- frontend/components/home/logo.jsx
+++ frontend/components/home/logo.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import img from '../../images/mount.jpg'
+import img from '../../images/soap.png'
 
 function Logo() {
     const logo = "Whats good"
 
     return (
-        <div>
-            <img src={img} />
+        <div className={"group nav-logo-container"}>
+            <img className={"nav-logo-image"} src={img} />
             <Link className={"nav-logo"} to="/index">Soapbox</Link>
         </div>
     )
